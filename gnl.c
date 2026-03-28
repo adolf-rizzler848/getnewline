f8e864acc5c6240b176a4e236865ba0c9d5aee3b
Mayank <MAYANKJOSHI.25101280629@gehu.ac.in>
2026-03-28 11:21:09 +0000

    phase

--- gnl.c
+++ gnl.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFSIZE 2048
+char *zero(char*);
 int main(void);
-char *gnl(void);
+char *zero(char *p){
+    unsigned int n;
+    for(n=0; n<BUFSIZE; n++){
+        *(p + n) = 0;
+    }
+
+    return (char *)p;
+}
+char *gnl(void){
+    char *p, *s;
+    int r;
+    unsigned int n;
+    n=0;
+    s = p = malloc(BUFSIZE);
+    zero(&s);
+    //read system call one at a time
+    while(1){
+        r = read(0, p, 1);
+        if(r<1){
+            if(n<1){
+                return 0;
+            }
+            else{
+                return s;
+            }
+        }
+        else if (n>(BUFSIZE-2)) {
+            return s;
+        }
+    }
+
+
+
+
+
+
+}
 int main(){
     char *p1, *p2;
     p1=gnl();
